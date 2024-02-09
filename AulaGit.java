69e10d9076a5038ace94208ace7cfc0ca1e45c8a
Gabriela Andrade <gabrielav.sudo@gmail.com>
2024-02-08 21:29:26 -0300

    Update AulaGit.java

--- AulaGit.java
+++ AulaGit.java
@@ -1,5 +1,5 @@
 public class AulaGit {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! Acaba logo que eu quero ir pro bloco!");
     }
 }
